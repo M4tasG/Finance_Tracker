3a839f1c7ff4537cd6771babba6bd48a6126f1e6
M4tasG <grybauskas.matas@gmail.com>
2021-09-07 10:46:23 +0300

    added config.json

--- Finance_Tracker/settings.py
+++ Finance_Tracker/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's06yu0%c3%&k66&ge9@k^56o98*$cc+9%agobw-6bnue+elby)'
+SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
